d1db27330f89437935c87872d91f6d818f3e4598
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-05-26 01:40:26 +0300

    Update ProductItem.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -3,7 +3,7 @@ import { Button, CardContainer, CardTitle, Image } from "./ProductItem.styled";
 export const ProductItem = ({ name, photo, price, handleAddToCart, id }) => {
   return (
     <CardContainer>
-      <Image src={photo} alt={name} width={200} height={100} />
+      <Image src={photo} alt={name} width="200px" height="100px" />
       <CardTitle>{name}</CardTitle>
       <CardTitle>Price: {price}</CardTitle>
       <Button type="button" onClick={() => handleAddToCart(id)}>
